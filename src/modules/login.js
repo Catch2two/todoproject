8dbc65868c93f365a70a14fe3a30f4fa16dc8a77
Josh <118862870+Catch2two@users.noreply.github.com>
2023-06-19 15:28:24 -0400

    Finally added password error to show up

--- src/modules/login.js
+++ src/modules/login.js
@@ -1,27 +1,34 @@
 const email = document.getElementById("email");
 const emailError = document.querySelector("#email + span.error");
 const password = document.getElementById("password")
-const passError = document.querySelector("#password + span.error");
-const form = document.querySelector("login");
+const passwordError = document.querySelector("#password + span.error");
+
+function showError() {
+    if (email.validity.valueMissing) {
+        emailError.textContent = "Enter an email";
+    } else if (email.validity.typeMismatch) {
+        emailError.textContent = "Please enter a valid Email"
+    } else if (email.validity.tooShort) {
+        emailError.textContent = `Email should be at least ${email.minLength} characters; you entered ${email.value.length}.`;
+    } else if (password.validity.tooShort) {
+        passwordError.textContent = "Password should be at least 8 characters";
+      }
+}
 
 email.addEventListener("input", (e) => {
-    if (email.validity.valid && password.validity.valid) {
+    if (email.validity.valid) {
         emailError.textContent = "";
-        emailError.className = "hidden";
-        passError.textContent = "";
-        passError.className = "hidden";
-      } else {
+        emailError.className = "error"
+    } else {
         showError()
     }
 });
 
-
-function showError() {
-    if (password.validity.valueMissing) {
-        passError.textContent = "Enter a password";
-    } else if (password.validity.typeMismatch) {
-        passError.textContent = "Please enter a valid password"
-    } else if (password.validity.tooShort) {
-        passError.textContent = `Password should be at least ${password.minLength} characters; you entered ${password.value.length}.`;
+password.addEventListener("input", (e) => {
+    if (password.validity.valid) {
+        passwordError.textContent = "";
+        passwordError.className = "error"
+    } else {
+        showError()
     }
-}
+});

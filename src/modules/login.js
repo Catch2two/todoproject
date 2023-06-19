4a5cb9280c466b85dd3c668bd9c9b36c2fe7a0dc
Josh <118862870+Catch2two@users.noreply.github.com>
2023-06-19 14:52:29 -0400

    resizing

--- src/modules/login.js
+++ src/modules/login.js
@@ -1,18 +1,27 @@
 const email = document.getElementById("email");
+const emailError = document.querySelector("#email + span.error");
 const password = document.getElementById("password")
+const passError = document.querySelector("#password + span.error");
+const form = document.querySelector("login");
 
 email.addEventListener("input", (e) => {
-    if (email.validity.typeMismatch) {
-        email.setCustomValidity("Enter a valid email address.");
-    } else {
-        email.setCustomValidity("");
+    if (email.validity.valid && password.validity.valid) {
+        emailError.textContent = "";
+        emailError.className = "hidden";
+        passError.textContent = "";
+        passError.className = "hidden";
+      } else {
+        showError()
     }
 });
 
-password.addEventListener("input", (e) => {
-    if (password.validity.typeMismatch) {
-        password.setCustomValidity("Enter a valid password.");
-    } else {
-        password.setCustomValidity("");
+
+function showError() {
+    if (password.validity.valueMissing) {
+        passError.textContent = "Enter a password";
+    } else if (password.validity.typeMismatch) {
+        passError.textContent = "Please enter a valid password"
+    } else if (password.validity.tooShort) {
+        passError.textContent = `Password should be at least ${password.minLength} characters; you entered ${password.value.length}.`;
     }
-});
+}
